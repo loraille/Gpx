e3140c412d5a3bc3f33dba1f40aeb8827d87f285
loraille <lo.rail@proton.me>
2024-12-23 10:05:29 +0100

    ajout tracks excideuil

--- scripts/tracks.js
+++ scripts/tracks.js
@@ -1,11 +1,17 @@
 // Groupes de traces avec plusieurs fichiers GPX par groupe
 window.vttGPX = [
     {
-        gpxFiles: ['https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1732872486/vtt-cr1_ixdejm.gpx','https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1732872485/vtt-cr2_nfqqt9.gpx'],
+        gpxFiles: ['https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1732872486/vtt-cr1_ixdejm.gpx','https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1732872485/vtt-cr2_nfqqt9.gpx']
     },
     {
-        gpxFiles: ['https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734041669/vtt-sp1_k5svja.gpx','https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734043624/vtt-sp2_vdqsw2.gpx'],
+        gpxFiles: ['https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734041669/vtt-sp1_k5svja.gpx','https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734043624/vtt-sp2_vdqsw2.gpx','https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734282591/vtt-sp3_yvv0za.gpx','https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734286648/vtt-sp4_udcqvv.gpx'],
     },
+    {
+        gpxFiles: ['https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734195169/vtt-chpr1_it4umv.gpx']
+    },
+    {
+        gpxFiles: ['https://res.cloudinary.com/dj6cmwlv8/raw/upload/v1734943921/vtt-ex1_fnuhjw.gpx']
+    }
 ];
 window.courseGPX = [
     {
